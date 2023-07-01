fa36ff4485893c07536a7848e029b663fa17fad6
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-07-01 16:25:10 -0300

    terminando capitulo sobre views

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -13,6 +13,10 @@ class SeriesController extends Controller
             'Grey\'s Anatomy'
         ];
 
-       return view('listar-series', compact('series'));
+       return view('series.index')->with('series',$series);
+    }
+
+    public function create(){
+        return view('series.create');
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,15 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Series</title>
+    <title>{{$title}} - Controle de séries</title>
 </head>
 <body>
     <h1>Séries</h1>
-    <ul>
-        <?php foreach($series as $serie):?>
-        <li><?= $serie; ?></li>
-        <?php endforeach;?>
-    </ul>
+
+    {{$slot}}
+
 </body>
 </html>
 
