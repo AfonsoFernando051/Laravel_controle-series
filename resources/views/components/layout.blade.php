2c6a077ab852efb53640a6d4765daadce4b66b3d
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-07-02 20:49:56 -0300

    finalizando primeiro curso de laravel

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}} - Controle de séries</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    <h1>Séries</h1>
+    <div class="container">
+        <h1>Séries</h1>
 
-    {{$slot}}
-
+        {{$slot}}
+    </div>
 </body>
 </html>
 
